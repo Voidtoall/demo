8d9ab417b4f897d12a4367212925b969a767b106
zhangyaohai@sina.com <zhangyaohai@sina.com>
2024-04-28 17:21:23 +0800

    fix run error

--- controller/src/main/java/com/example/config/SpringConfiguration.java
+++ controller/src/main/java/com/example/config/SpringConfiguration.java
@@ -3,21 +3,12 @@ package com.example.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
-import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -25,8 +16,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(value = "com.example.*")
@@ -36,12 +25,7 @@ import javax.sql.DataSource;
 @EnableScheduling
 @EnableAutoConfiguration
 @EnableCaching(proxyTargetClass = true)
-@EnableProcessApplication
 public class SpringConfiguration implements WebMvcConfigurer {
-
-
-    @Autowired
-    private RuntimeService runtimeService;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -68,10 +52,5 @@ public class SpringConfiguration implements WebMvcConfigurer {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));//分页插件
         return interceptor;
-    }
-
-    @EventListener
-    public void processPostDeploy(PostDeployEvent event) {
-        runtimeService.startProcessInstanceByKey("loanApproval");
     }
 }
